198f521357ce9e3972d75c2ca4de8c8f9e086bc0
Alexei <bitskoff@gmail.com>
2019-11-26 00:45:50 +0800

    added checking number

--- Time/main.cpp
+++ Time/main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
 #include "Time.h"
 
-int main() {
-    Time t1(1, 20, 35);
-    Time t2(0, 0, 0);
+using namespace std;
 
+bool is_number(const string & str) {
+    int point = 0;
+
+    bool started = false;
+    bool warn = false;
+
+    for (char ch : str) {
+        if (ch == ' ') {
+            if (started) {
+                warn = true;
+            }
+        } else {
+            if (warn) {
+                return false;
+            }
+
+            started= true;
+            if (ch == '.') {
+                started = true;
+                point += 1;
+            } else if (!(ch >= '0' && ch <= '9')) {
+                return false;
+            }
+        }
+    }
+    return point <= 1;
+}
+
+
+int main() {
+    string input;// = "AB.10";
+    getline(cin,input);
+    cout << is_number(input) << endl;
     return 0;
 }
